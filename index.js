bb7a1cee31c16cc07b8d0c9e4624d5f3fbc7fbdd
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-16 15:13:01 +1100

    callbacks - error first callbacks

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function processInput (input, callback){
-    callback(input)
+function doMath(num1, num2, callback){
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        const err = new Error("Can only perform math on numbers")
+        callback(err)
+        return
+    }
+    callback(null, num1,num2)
 }
 
-function greet(name) {
-    console.log(`Hello ${name}`)
+function multiply(err,a,b){
+    if (err){
+        return console.error(err.message)
+    }
+    console.log(a * b)
 }
 
-function excitedGreet(name) {
-    console.log ("HI THERE " + name.toUpperCase() + "!!!")
+function add(err, x,y){
+    if (err){
+        return console.error(err.message)
+    }
+    console.log(x + y)
 }
 
-processInput("Alex", greet)
-processInput("John", excitedGreet)
+doMath(2,5, multiply)
+doMath(3,3, add)
