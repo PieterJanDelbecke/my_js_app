5eb63a277b170da36ce14d4aed5d1baf17a15c8f
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-20 10:15:38 +1100

    switch cases

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var saying = 'home sweet home'
+let favouriteColour = "pink"
 
-console.log(saying.toUpperCase())
-console.log(saying.toLowerCase())
-console.log(saying[0].toUpperCase())
-console.log(saying.indexOf("e"))
-console.log(saying.lastIndexOf("e"))
-console.log(saying.substring(1))
-console.log(saying.substring(1,4))
-console.log(saying.split(" "))
-console.log(saying.trim())
-console.log(saying.replace("sweet","dirty"))
+switch(favouriteColour){
+    case "red":
+        console.log("Blood")
+        break
+    case "yellow":
+        console.log("Spirits")
+        break
+    default:
+        console.log("NOT HERE!")
+        break
+}
