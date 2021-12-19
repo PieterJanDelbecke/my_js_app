ea8d89a2b5098e165c0192712d6b4feafc3a18dd
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-20 09:57:28 +1100

    string methods

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let numbers = ["one","two","three"]
+var saying = 'home sweet home'
 
-let two = numbers.splice(1,1)
-console.log(numbers)
-console.log(two)
+console.log(saying.toUpperCase())
+console.log(saying.toLowerCase())
+console.log(saying[0].toUpperCase())
+console.log(saying.indexOf("e"))
+console.log(saying.lastIndexOf("e"))
+console.log(saying.substring(1))
+console.log(saying.substring(1,4))
+console.log(saying.split(" "))
+console.log(saying.trim())
+console.log(saying.replace("sweet","dirty"))
