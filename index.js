4cfb1c2e7b9b175e6225d247536f4df1c96da360
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-21 10:20:41 +1100

    promise chaining

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-function squareNumber(number){
-    return new Promise((resolve,reject)=>{
-        if (typeof number !=='number'){
-            reject(new Error("Input must be a number"))
-        }
-        resolve(number * number)
+function generateRandomNumber(limit){
+    console.log("Generating number between 1-"+limit)
+    return new Promise((resolve,reject) => {
+        setTimeout(()=>{
+            if(typeof limit !== 'number'){
+                reject( new Error("Input must be a number"))
+            }
+            const randomNumber = Math.floor(Math.random()*limit)+1
+            resolve(randomNumber)
+        }, 1000)
     })
 }
+function doubleNumber(num){
+    return new Promise ((resolve,reject) => {
+        if(num < 0){
+            reject(new Error("Can't be negative"))
+        }
+        resolve(num *2)
+    })
+}
+function logIfSmall(num){
+    if(num > 15){
+        throw new Error ("That number is too big")
+    } else
+    console.log("The doubled number is "+ num)
+}
 
-squareNumber("10")
-    .then(sqauredNumber => console.log("The sauared number is " + sqauredNumber))
-    .catch(error => console.log(error.message))
-    .finally(() => console.log("The promise has finished"))
+generateRandomNumber(100)
+    .then(number =>{
+        console.log("The number is "+ number)
+        return number
+    })
+    .then(doubleNumber)
+    .then(logIfSmall)
+    .catch(error => console.error("Caught error: " + error.message))
