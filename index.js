77499751fa0bc0cd1310f184650f3a1275936f23
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-21 08:01:54 +1100

    Promises then/catch/finally

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-let myFirstPromise = Promise.resolve("My first promise, yay!")
-myFirstPromise.then(function(value){
-    console.log(value + "!!!")
-})
+function squareNumber(number){
+    return new Promise((resolve,reject)=>{
+        if (typeof number !=='number'){
+            reject(new Error("Input must be a number"))
+        }
+        resolve(number * number)
+    })
+}
+
+squareNumber("10")
+    .then(sqauredNumber => console.log("The sauared number is " + sqauredNumber))
+    .catch(error => console.log(error.message))
+    .finally(() => console.log("The promise has finished"))
