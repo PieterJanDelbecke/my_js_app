5898f0d122f6865eea16ed28bd5010f55a861f5d
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-24 09:23:19 +1100

    CodeWars - median

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function sayHi(name){
-    console.log(name[0].toUpperCase()+ name.substring(1))
+function median (array){
+    array.sort((a,b)=> a-b)
+    let i = Math.floor(array.length/2)
+    if ((array.length%2) == 0) {
+        return (array[i-1] + array[i])/2
+    } else {
+        return array[i]
+    }
 }
 
-function greet(name, callback){
-    callback(name)
-}
 
-greet('emma', sayHi)
+console.log(median([3,2,1]))
+console.log(median([3,2,1,7,9]))
+console.log(median([33,99,100,30,29,50]))
