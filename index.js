8bdd18173598fa4d2a076bb88155185f9472eb08
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-24 11:15:30 +1100

    if-else statements'

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-function median (array){
-    array.sort((a,b)=> a-b)
-    let i = Math.floor(array.length/2)
-    if ((array.length%2) == 0) {
-        return (array[i-1] + array[i])/2
+function evaluateNum (num){
+    if (typeof num === 'number'){
+        if (num > 100){
+            console.log(`${num} is greater than 100`)
+        }else if(num === 100){
+            console.log(`${num} is equal to 100`)
+        }else {
+            console.log(`${num} is smaller than 100`)
+        }
     } else {
-        return array[i]
+        console.log(`${num} is not a number`)
     }
+
 }
 
+evaluateNum(101)
+evaluateNum(99)
+evaluateNum("a")
 
-console.log(median([3,2,1]))
-console.log(median([3,2,1,7,9]))
-console.log(median([33,99,100,30,29,50]))
