b0dc09a5f474fdfaa17e79688b683c6ce3666246
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-24 13:03:48 +1100

    while loops

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-function stringOrNumber(input){
-    let dataType = typeof input
-    switch(dataType){
-        case 'number':
-            console.log(`${input} is a number`)
-            break
-        case 'string':
-            console.log(`${input} is a string`)
-            break
-        default:
-            console.log(`The input is not a number or string`)
-            break
+
+function loops(input){
+    let condition = true
+    while (condition){
+        num = Math.floor(Math.random() * 5 + 1)
+        if (num === input){
+            console.log(`YEAH, random number: ${num}, is equal to input number: ${input}`)
+            condition = false
+        } else {
+            console.log(`NO, random number: ${num} and input number: ${input} are not the same`)
+        }
     }
 }
 
-stringOrNumber(100)
-stringOrNumber('Hello')
-stringOrNumber([1,5,99])
-stringOrNumber({firstName:'Adam', lastname: 'Ant'})
-
+loops(4)
