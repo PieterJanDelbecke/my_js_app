2e82595e4eda1b35556be01af87f70bf6a6028c3
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-27 11:11:36 +1100

    sorting algorithms

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-let arr = ["a","b","c","d"]
-let arr2 = arr.splice(1,2)
-console.log(arr)        // will return ["a","d"]
-console.log(arr2)       // will return ["c","d"]
+let arr1 = [2, 1, 88, 111, 201,3]
+let arr2 = arr1.sort()
+console.log(arr2)   // will return [ 1, 111, 2, 201, 3, 88 ]
+
+
+let arr3 = arr1.sort((a,b)=> a - b)     // when a < b : will return a negative value
+                                        // when a > b : will return a positive value
+                                        // when a = b : will return 0
+
+console.log(arr3)   // [ 1, 2, 3, 88, 111, 201 ]
