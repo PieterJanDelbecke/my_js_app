3dd011c3eaa8b37ed49d0a02ab67bf2618352c46
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-28 09:02:39 +1100

    prototypes

--- index2.js
+++ index2.js
@@ -7,7 +7,7 @@ function Person(name, age){
 let instructor = new Person ('Pieter-Jan','40')
 
 
-function Hero (name,level){
+function Hero(name,level){
     this.name = name;
     this.level = level;
     this.shout = function (){
@@ -15,5 +15,23 @@ function Hero (name,level){
     }
 }
 
-let trooper = new Hero("King Kong", 4)
-console.log(trooper.shout())
+// let trooper = new Hero("King Kong", 4)
+// // console.log(trooper.shout())
+
+// let student = new Person ("Bo",22)
+// Person.prototype.role = 'student'
+// let newStudent = new Person ("Joe",25)
+// console.log(newStudent.role)
+
+function Hero2(name,level){
+    this.name = name
+    this.level = level
+}
+Hero2.prototype.shout = function(){
+    return `my name is ${this.name}!`
+}
+
+let pieter = new Hero("Pieter", 40)
+let alessandra = new Hero2("Ale",38)
+console.log(pieter.shout())
+console.log(alessandra.shout())
