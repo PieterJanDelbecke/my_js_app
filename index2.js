46bb97c23d82697bfdd1e735635774553659473a
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-12-29 15:57:01 +1100

    Two Oldet Ages

--- index2.js
+++ index2.js
@@ -1,7 +1,16 @@
-function incrementer(nums) { 
-    return nums.map((value,index)=>{
-            return (value + index + 1)%10
+function twoOldestAges(ages){
+    let arr = [0,0]
+    ages.forEach(element => {
+        if (element > arr[1]){
+            arr[0] = arr[1]
+            arr[1] = element
+        } else if ( element > arr[0]){
+            arr[0] = element
+        }
     })
-  }
+    return arr
+}
 
-console.log(incrementer([3, 6, 9, 8, 9]))
+console.log(twoOldestAges([1,5,87,45,8,8]))
+console.log(twoOldestAges([6,5,83,5,3,18]))
+console.log(twoOldestAges([64,34,58,12,86,94,1,72,67,96,48,45,21,59,92,19,24,86,7,39,23,48,39,5,33,46,52,25,89]))
